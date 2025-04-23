9fada69064400f8915296f760ae93b8dd139bf0f
DerFl3x <d3rflex@proton.me>
2025-04-23 20:02:07 +0200

    init

---
+++ src/main/java/com/tesefire/dashy/DashyApplication.java
@@ -0,0 +1,13 @@
+package com.tesefire.dashy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tesefire/dashy/DashyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesefire.dashy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
