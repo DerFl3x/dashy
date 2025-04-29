faafa95bb62ee5a3bf808875b5ba6355637112e5
Alexander Frühauf <alexander.fruehauf@isys.de>
2025-04-29 11:17:51 +0200

    Use @Value annotation to inject the weather API key
    Replaced the hardcoded API key with a dynamically injected value using the @Value annotation. This improves flexibility and security by allowing the key to be managed via configuration.

--- src/main/java/com/tesefire/dashy/controller/WeatherController.java
+++ src/main/java/com/tesefire/dashy/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package com.tesefire.dashy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api")
 public class WeatherController {
 
+    @Value("${weather.api.key}")
+    String apiKey;
+
     @GetMapping("/weather")
     public String liveWeather() {
-        String apiKey = "8d112487cfda56b863244a3bceb68500";
+
         String city = "Munich";
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
 
